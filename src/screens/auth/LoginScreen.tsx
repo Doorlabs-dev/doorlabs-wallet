01c06d81ea3a299ca13360506f6f75dfcbf19439
mthinh <thinh@coderpush.com>
2022-08-02 18:22:40 +0700

    handle submit login

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -3,34 +3,49 @@ import { Container, Spacer } from '../../components/layout';
 import useBiometrics from '../../hooks/useBiometrics';
 import { Button, Text, Title } from '../../components/ui';
 import { TextInput } from '../../components/form';
-import { Alert } from 'react-native';
+import { ActivityIndicator, Alert } from 'react-native';
 import useWalletPassword from '../../services/wallet_password';
 import useAuthentication from '../../hooks/useAuthentication';
 import { Controller, useForm } from 'react-hook-form';
+import wallet from '../../services/wallet';
+import { colors } from '../../styles';
 
 const LoginScreen = () => {
   const { authenticateBiometrics, isBiometricsAvailable } = useBiometrics();
-  const { getSavedPassword } = useWalletPassword();
+  const { getWalletSavedPassword } = useWalletPassword();
   const { setIsAuthenticated } = useAuthentication();
   const [savedPassword, setSavedPassword] = useState<string | null>();
+  const [isStarting, setIsStarting] = useState(false);
 
   const { control, handleSubmit } = useForm({
     defaultValues: { password: '' },
   });
 
   useEffect(() => {
-    getSavedPassword().then((value) => setSavedPassword(value));
+    getWalletSavedPassword().then((value) => setSavedPassword(value));
   }, []);
 
   const onSubmit = () => {
-    handleSubmit(({ password }) => {
-      if (password === savedPassword) {
-        setIsAuthenticated(true);
-      } else {
-        Alert.alert('Wrong password');
-      }
+    handleSubmit(async ({ password }) => {
+      if (password !== savedPassword) return Alert.alert('Wrong password');
+
+      if (wallet.hasSession()) return setIsAuthenticated(true);
+
+      setIsStarting(true);
+
+      const success = await wallet.startSession(password);
+
+      setIsStarting(false);
+      if (success) return setIsAuthenticated(true);
     })();
   };
+
+  if (isStarting)
+    return (
+      <Container>
+        <ActivityIndicator size="large" color={colors.white} />
+      </Container>
+    );
 
   return (
     <Container>
