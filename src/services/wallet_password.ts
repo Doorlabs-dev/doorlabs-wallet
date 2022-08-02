0ceae678c3cc9f1f222d121029d9528b4d3a7747
mthinh <thinh@coderpush.com>
2022-08-02 18:18:40 +0700

    remove unused code in wallet service

--- src/services/wallet_password.ts
+++ src/services/wallet_password.ts
@@ -3,23 +3,23 @@ import * as SecureStore from 'expo-secure-store';
 const WALLET_PASSWORD_KEY = 'wallet_password';
 
 const useWalletPassword = () => {
-  const getSavedPassword = async () => {
+  const getWalletSavedPassword = async () => {
     const value = await SecureStore.getItemAsync(WALLET_PASSWORD_KEY);
     return value;
   };
 
-  const setPassword = async (password: string) => {
+  const setWalletPassword = async (password: string) => {
     await SecureStore.setItemAsync(WALLET_PASSWORD_KEY, password);
   };
 
-  const deletePassword = async () => {
+  const deleteWalletPassword = async () => {
     await SecureStore.deleteItemAsync(WALLET_PASSWORD_KEY);
   };
 
   return {
-    setPassword,
-    getSavedPassword,
-    deletePassword,
+    getWalletSavedPassword,
+    setWalletPassword,
+    deleteWalletPassword,
   };
 };
 
