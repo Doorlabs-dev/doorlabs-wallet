fe79a7897d83a1f04017022c726bee76fcd83dc4
mthinh <thinh@coderpush.com>
2022-08-02 18:20:48 +0700

    use Drawer navigator for main flow

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,6 @@ import Button from './Button';
 import ShortAddress from './ShortAddress';
 import Text from './Text';
 import Title from './Title';
+import EtherIcon from './EtherIcon';
 
-export { Title, Button, Text, ShortAddress };
+export { Title, Button, Text, ShortAddress, EtherIcon };
