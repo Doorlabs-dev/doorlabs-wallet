94dbd88fee7fe592dfc26ffe75a0bddd280d8082
mthinh <thinh@coderpush.com>
2022-08-21 15:51:23 +0700

    add account screen header

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,9 +2,11 @@ const primary = '#161616';
 const secondary = '#383838';
 const greyScale = '#414141';
 const lightGreyScale = '#888888';
+const whiteGrey = '#CFCFCF';
 const white = '#ffffff';
 const red = 'red';
 const orange = '#FF6F36';
+const blue = '#3092FC';
 const darkBlue = 'rgba(93, 100, 214, 0.1)';
 const middleBlue = 'rgba(93, 100, 214, 0.3)';
 const lightBlue = 'rgba(93, 100, 214, 0.9)';
@@ -15,9 +17,11 @@ export default {
   secondary,
   greyScale,
   lightGreyScale,
+  whiteGrey,
   white,
   red,
   orange,
+  blue,
   darkBlue,
   middleBlue,
   lightBlue,
