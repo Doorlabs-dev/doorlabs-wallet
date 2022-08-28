9924b28f5f848e3961fdc99a9a9eab1950b313d3
mthinh <thinh@coderpush.com>
2022-08-28 20:22:50 +0700

    transform tx list to section list to render by date

--- src/features/account/screen/AccountActivityScreen.tsx
+++ src/features/account/screen/AccountActivityScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { SectionList } from 'react-native';
 import React from 'react';
 import { Container, SafeArea, Spacer } from '@components/layout';
 import useAccountTransactions from '@features/transactions/hooks/useAccountTransactions';
@@ -6,6 +6,10 @@ import { useRecoilValue } from 'recoil';
 import selectedAccountState from '../selected-account.state';
 import TransactionItem from '@features/transactions/components/TransactionItem';
 import { FlatList } from 'react-native-gesture-handler';
+import { forOwn, groupBy } from 'lodash-es';
+import { getTransactionsBySection } from '../utils/getTransactionsBySection';
+import { Text } from '@components/ui';
+import colors from '@styles/colors';
 
 type Props = {};
 
@@ -14,17 +18,28 @@ const AccountActivityScreen = (props: Props) => {
   const { sortedTransactions } = useAccountTransactions({
     account: selectedAccount,
   });
+
+  const transactionsSections = getTransactionsBySection(sortedTransactions);
+
   return (
     <SafeArea>
       <Container center={false}>
-        <FlatList
+        <SectionList
           ItemSeparatorComponent={() => <Spacer height={16} />}
           contentContainerStyle={{ flex: 1 }}
           renderItem={({ item: tx }) => (
             <TransactionItem key={tx.hash} transaction={tx} />
           )}
+          renderSectionHeader={({ section }) => (
+            <>
+              <Text color={colors.white} size={16} lineHeight={24} weight={500}>
+                {section.title}
+              </Text>
+              <Spacer height={16} />
+            </>
+          )}
           keyExtractor={(tx) => tx.hash}
-          data={sortedTransactions}
+          sections={transactionsSections}
         />
       </Container>
     </SafeArea>
