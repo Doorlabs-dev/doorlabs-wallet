695b1aaf0b72c494fdb2cb7eda682f7db82ca104
mthinh <thinh@coderpush.com>
2022-09-02 20:06:41 +0700

    add getSupportTypes and getAuthenticationTypeName in useBiometrics

--- src/hooks/useBiometrics.ts
+++ src/hooks/useBiometrics.ts
@@ -1,19 +1,45 @@
 import { Alert } from 'react-native';
 import * as LocalAuthentication from 'expo-local-authentication';
 import { useEffect, useState } from 'react';
+import LocalStorage from '@services/store/localstore';
+
+export const biometricsStore = new LocalStorage<boolean>({
+  name: 'walletme:usedBiometrics',
+  defaultValue: false,
+});
 
 type UseBiometrics = {
   isBiometricsAvailable: boolean;
-  authenticateBiometrics: (onSuccess?: () => void) => Promise<void>;
+  authenticateBiometrics: () => Promise<boolean>;
   checkAvailable: () => Promise<boolean>;
+  supportedTypes: LocalAuthentication.AuthenticationType[];
+  getAuthenticationTypeName: () => string;
 };
 
 const useBiometrics = (): UseBiometrics => {
   const [isBiometricsAvailable, setIsBiometricsAvailable] = useState(false);
+  const [supportedTypes, setSupportedTypes] = useState<
+    LocalAuthentication.AuthenticationType[]
+  >([]);
 
   useEffect(() => {
     checkAvailable().then(setIsBiometricsAvailable);
   }, []);
+
+  useEffect(() => {
+    if (isBiometricsAvailable) {
+      LocalAuthentication.supportedAuthenticationTypesAsync().then((value) => {
+        setSupportedTypes(value);
+      });
+    }
+  }, [isBiometricsAvailable]);
+
+  const getAuthenticationTypeName = (): string => {
+    return supportedTypes[supportedTypes.length - 1] ===
+      LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION
+      ? 'FaceID'
+      : 'Fingerprint/Pass Code';
+  };
 
   const checkAvailable = async () => {
     const isHardwareAvailable = await LocalAuthentication.hasHardwareAsync();
@@ -21,21 +47,18 @@ const useBiometrics = (): UseBiometrics => {
     return isHardwareAvailable && isEnrolled;
   };
 
-  const authenticateBiometrics = async (onSuccess?: () => void) => {
+  const authenticateBiometrics = async (): Promise<boolean> => {
     const result = await LocalAuthentication.authenticateAsync();
 
-    if (result.success) {
-      Alert.alert('Sucessfully authenticated');
-      onSuccess?.();
-    } else {
-      Alert.alert('Fail authentication');
-    }
+    return result.success;
   };
 
   return {
     isBiometricsAvailable,
+    supportedTypes,
     authenticateBiometrics,
     checkAvailable,
+    getAuthenticationTypeName,
   };
 };
 
