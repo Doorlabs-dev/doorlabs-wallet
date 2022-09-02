f55684ec33495c298fe876fd85ca7b849f3a94a1
mthinh <thinh@coderpush.com>
2022-09-02 20:31:14 +0700

    add biometrics config to setting screen

---
+++ src/features/setting/components/BiometricsSetting.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import BiometricsEnableSwitch from '@features/biometrics/components/BiometricsEnableSwitch';
+import useBiometrics, { biometricsStore } from '@hooks/useBiometrics';
+
+type Props = {};
+
+const BiometricsSetting = (props: Props) => {
+  const [enabled, setEnabled] = useState(false);
+
+  const { supportedTypes } = useBiometrics();
+
+  const onChange = async (enabled: boolean) => {
+    setEnabled(enabled);
+    await biometricsStore.set(enabled);
+  };
+
+  useEffect(() => {
+    biometricsStore.get().then((val) => setEnabled(val || false));
+  }, []);
+
+  return (
+    <BiometricsEnableSwitch
+      supportedTypes={supportedTypes}
+      enabled={enabled}
+      onChange={onChange}
+    />
+  );
+};
+
+export default BiometricsSetting;
