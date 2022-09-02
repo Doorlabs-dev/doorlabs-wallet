5917e06355301b93fb1d5db694d8bc6e323ad492
mthinh <thinh@coderpush.com>
2022-09-02 14:30:50 +0700

    add scroll view for new wallet screen

--- src/features/onboarding/screen/NewWalletScreen.tsx
+++ src/features/onboarding/screen/NewWalletScreen.tsx
@@ -1,10 +1,24 @@
 import { Checkbox } from '@components/ui/Checkbox';
+import { StackActions, useNavigation } from '@react-navigation/native';
+import { ScreenNavigationProps } from '@router/navigation-props';
+import ScreenNames from '@router/screenNames';
 import React, { useState } from 'react';
 import { Controller, FieldValues, useForm } from 'react-hook-form';
-import { ActivityIndicator, StyleSheet, Switch, View } from 'react-native';
+import {
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+  Switch,
+  View,
+} from 'react-native';
 import Toast from 'react-native-root-toast';
 import { TextInput } from '../../../components/form';
-import { Container, SafeArea, Spacer } from '../../../components/layout';
+import {
+  Container,
+  KeyboardView,
+  SafeArea,
+  Spacer,
+} from '../../../components/layout';
 import { Button, Text, Title } from '../../../components/ui';
 import wallet from '../../../services/wallet';
 import useWalletPassword from '../../../services/wallet_password';
@@ -29,8 +43,9 @@ const NewWalletScreen = () => {
   });
 
   const { setWalletPassword } = useWalletPassword();
-  const { setIsAuthenticated, setIsAccountAvailable } = useAuthentication();
+
   const [isCreatingWallet, setIsCreatingWallet] = useState(false);
+  const navigation = useNavigation<ScreenNavigationProps<any>>();
 
   const onConfirm = () => {
     handleSubmit(async ({ password }) => {
@@ -42,10 +57,7 @@ const NewWalletScreen = () => {
 
         if (!success) throw Error('Error creating wallet');
 
-        setTimeout(() => {
-          setIsAuthenticated(true);
-          setIsAccountAvailable(true);
-        }, 300);
+        navigation.dispatch(StackActions.replace(ScreenNames.SECURE_WALLET));
       } catch (e) {
         setIsCreatingWallet(false);
         Toast.show(`${e}`);
@@ -72,70 +84,79 @@ const NewWalletScreen = () => {
 
   return (
     <SafeArea>
-      <Container style={{ justifyContent: 'flex-start' }}>
-        <Title textAlign="center" size={28}>
-          New password for your wallet
-        </Title>
-        <Spacer height={24} />
-        <TextInput
-          name="password"
-          placeholder="Password"
-          label="Password"
-          control={control}
-          rules={{ required: 'This field is required!' }}
-          inputProps={{
-            secureTextEntry: true,
-          }}
-        />
-        <TextInput
-          name="passwordConfirmation"
-          placeholder="Password confirmation"
-          label="Confirm password"
-          control={control}
-          rules={{
-            required: 'This field is required!',
-            validate: isSameValue,
-          }}
-          inputProps={{
-            secureTextEntry: true,
-          }}
-        />
-        <Spacer height={16} />
-        <Button onPress={onConfirm} width={'100%'}>
-          <Title size={16}>Confirm</Title>
-        </Button>
-        <View style={styles.faceIdSwitch}>
-          <Text size={16} color="white">
-            Unlock with FaceID
-          </Text>
-          <Controller
-            control={control}
-            name="useFaceId"
-            render={({ field: { value } }) => (
-              <Switch
-                value={value}
-                onChange={() => setValue('useFaceId', !value)}
+      <KeyboardView>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <Spacer height={36} />
+          <Container style={{ justifyContent: 'flex-start' }}>
+            <Title textAlign="center" size={28}>
+              New password for your wallet
+            </Title>
+            <Spacer height={24} />
+            <TextInput
+              name="password"
+              placeholder="Password"
+              label="Password"
+              control={control}
+              rules={{ required: 'This field is required!' }}
+              inputProps={{
+                secureTextEntry: true,
+              }}
+            />
+            <TextInput
+              name="passwordConfirmation"
+              placeholder="Password confirmation"
+              label="Confirm password"
+              control={control}
+              rules={{
+                required: 'This field is required!',
+                validate: isSameValue,
+              }}
+              inputProps={{
+                secureTextEntry: true,
+              }}
+            />
+            <Spacer height={16} />
+            <Button onPress={onConfirm} width={'100%'}>
+              <Title size={16}>Confirm</Title>
+            </Button>
+            <View style={styles.faceIdSwitch}>
+              <Text size={16} color="white">
+                Unlock with FaceID
+              </Text>
+              <Controller
+                control={control}
+                name="useFaceId"
+                render={({ field: { value } }) => (
+                  <Switch
+                    value={value}
+                    onChange={() => setValue('useFaceId', !value)}
+                  />
+                )}
               />
-            )}
-          />
-        </View>
-        <View style={styles.privacyWrapper}>
-          <Controller
-            control={control}
-            name="agreedPrivacy"
-            rules={{ required: 'Need to accept the privacy!' }}
-            render={({ field: { value } }) => (
-              <Checkbox
-                checked={value}
-                onChange={() => setValue('agreedPrivacy', !value)}
+            </View>
+            <View style={styles.privacyWrapper}>
+              <Controller
+                control={control}
+                name="agreedPrivacy"
+                rules={{ required: 'Need to accept the privacy!' }}
+                render={({ field: { value } }) => (
+                  <Checkbox
+                    checked={value}
+                    onChange={() => setValue('agreedPrivacy', !value)}
+                  />
+                )}
               />
-            )}
-          />
-          <Text size={16} color="white" style={styles.privacyTxt}>
-            I have read and agreed to the Terms and Conditions
-          </Text>
-        </View>
-      </Container>
+              <Text size={16} color="white" style={styles.privacyTxt}>
+                I have read and agreed to the Terms and Conditions
+              </Text>
+            </View>
+            <Spacer height={50} />
+          </Container>
+        </ScrollView>
+      </KeyboardView>
     </SafeArea>
   );
 };
