07af20aaada8e39d250153fec85110d88a1c224e
mthinh <thinh@coderpush.com>
2022-09-02 14:30:22 +0700

    add keyboard avoiding view

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,5 +3,6 @@ import Row from './Row';
 import Column from './Column';
 import Spacer from './Spacer';
 import SafeArea from './SafeArea';
+import KeyboardView from './KeyboardView';
 
-export { Container, Spacer, Row, Column, SafeArea };
+export { Container, Spacer, Row, Column, SafeArea, KeyboardView };
