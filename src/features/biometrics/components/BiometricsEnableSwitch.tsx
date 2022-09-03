41ef5db2564b8143c5848f047d7b85725256c62e
mthinh <thinh@coderpush.com>
2022-09-03 11:17:43 +0700

    enhance render navigator

--- src/features/biometrics/components/BiometricsEnableSwitch.tsx
+++ src/features/biometrics/components/BiometricsEnableSwitch.tsx
@@ -14,7 +14,7 @@ type Props = {
 const BiometricsEnableSwitch = ({
   enabled = false,
   onChange = (value) => null,
-  supportedTypes,
+  supportedTypes = [],
 }: Props) => {
   if (!supportedTypes.length) return null;
 
