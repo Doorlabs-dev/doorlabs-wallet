90086c2d2b63377e1af946a8dd1b1c28862d443e
mthinh <thinh@coderpush.com>
2022-09-14 22:44:42 +0700

    mock action link

---
+++ src/features/walletconnect/mock_link.ts
@@ -0,0 +1,56 @@
+import { parseAmount } from '@services/tokens/amount';
+import { getUint256CalldataFromBN } from '@services/transaction';
+import * as Linking from 'expo-linking';
+import { getChecksumAddress, stark } from 'starknet';
+
+export function testConnectLink() {
+  const testConnectLink = Linking.createURL('connect-dapp', {
+    scheme: 'walletme',
+    queryParams: {
+      data: JSON.stringify({
+        meta: {
+          uri: 'mydapp',
+          name: 'Dapp Shit',
+        },
+        account: {
+          address:
+            '0xe3a491f1de4d976e183e0c1b0306c96d8951b0a0a2a8d36ec9619919157036',
+          networkId: 'goerli-alpha',
+        },
+      }),
+    },
+  });
+  Linking.openURL(testConnectLink);
+}
+
+export function testExecuteLink() {
+  const testConnectLink = Linking.createURL('execute-transaction', {
+    scheme: 'walletme',
+    queryParams: {
+      data: JSON.stringify({
+        meta: {
+          uri: 'mydapp',
+          name: 'Dapp Shit',
+        },
+        account: {
+          address:
+            '0xe3a491f1de4d976e183e0c1b0306c96d8951b0a0a2a8d36ec9619919157036',
+          networkId: 'goerli-alpha',
+        },
+        transactions: {
+          contractAddress:
+            '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7',
+          entrypoint: 'transfer',
+          calldata: stark.compileCalldata({
+            recipient: getChecksumAddress(
+              '0x001C64B40875EcE24c6E57136007F32FBc7eB25965A22629160aedbaA91A4f1a'
+            ),
+            amount: getUint256CalldataFromBN(parseAmount('0.0001')),
+          }),
+        },
+      }),
+    },
+  });
+
+  Linking.openURL(testConnectLink);
+}
