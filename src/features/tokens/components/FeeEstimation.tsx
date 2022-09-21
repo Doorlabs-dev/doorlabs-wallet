7dad85590e59ba8f3fbdaebc228bc32b1830e3e4
mthinh <thinh@coderpush.com>
2022-09-21 22:39:08 +0700

    useFeeEstimation to get network fee for tx, FeeEstimation component

---
+++ src/features/tokens/components/FeeEstimation.tsx
@@ -0,0 +1,70 @@
+import { ActivityIndicator, View } from 'react-native';
+import React, { useMemo } from 'react';
+import { Call } from 'starknet';
+import { getTokenInfo } from '@services/tokens';
+import { Account } from '@features/account/account.model';
+import { useBalance } from '../hooks/useBalance';
+import useFeeEstimation from '../hooks/useFeeEstimation';
+import { Text } from '@components/ui';
+import { Row } from '@components/layout';
+import colors from '@styles/colors';
+import { utils } from 'ethers';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  background-color: ${colors.greyScale800};
+  border-radius: 12px;
+  padding: 16px;
+`;
+
+type Props = {
+  account?: Account;
+  call: Call | Call[];
+  render?: (isEnough: boolean) => React.ReactNode;
+};
+
+const FeeEstimation = ({ account, call, render }: Props) => {
+  const feeToken = getTokenInfo('ETH', account?.networkId);
+
+  const { data: balance, error } = useBalance(feeToken?.address, account);
+  // fee as hex
+  const {
+    fee,
+    error: feeError,
+    isValidating: isFetchingFee,
+  } = useFeeEstimation(account, call);
+
+  const isEnough = useMemo(() => {
+    if (!!balance && !!fee) {
+      return balance.gte(fee);
+    }
+    return false;
+  }, [balance, fee]);
+
+  return (
+    <>
+      <Container>
+        <Row alignItems="center" justifyContent="space-between">
+          <View>
+            <Text size={16} color={colors.white}>
+              Network fee
+            </Text>
+            {!isEnough && !isFetchingFee && (
+              <Text>Not enough ETH for transaction</Text>
+            )}
+          </View>
+          {isFetchingFee ? (
+            <ActivityIndicator size="small" color={colors.white} />
+          ) : (
+            <Text size={16} color={colors.white}>
+              {utils.formatEther(fee || 0)} {feeToken?.symbol}
+            </Text>
+          )}
+        </Row>
+      </Container>
+      {render?.(isEnough && !isFetchingFee)}
+    </>
+  );
+};
+
+export default FeeEstimation;
